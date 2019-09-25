0b421850f6a771a135883c895c9daa78a433fad2
Berkan Cetinkaya <berkan.cetinkaya@osf-commerce.com>
2019-09-25 18:15:52 +0300

    adding new error

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-document.addEventListener("dblclick", function(){
+document.addEventListener("dblclick", function namedFunction(){
     throw new Error('Error')
 });
+
+var xbuttons = document.querySelectorAll("[data-bag]");
+
+for (var button of xbuttons) {
+    button.addEventListener('click',  function throwError() {
+        undeclaredVariable.something = 'something'
+    })
+}
